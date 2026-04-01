34e5f87284c806306497a1fae27438d36ed70cd0
obsidian <espino@apache.org>
2026-04-01 15:08:57 -0700

    feat: add hello world HTTP endpoint (hw-j29)
    Adds a Go HTTP server with a /hello endpoint that returns "Hello, World!"
    and a test for the handler.

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, World!")
+}
+
+func main() {
+	http.HandleFunc("/hello", helloHandler)
+	log.Println("Listening on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
